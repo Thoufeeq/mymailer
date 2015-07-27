14f4b92046ab20a70e6535bca2ab8b9517df9fd8
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2015-07-27 20:02:19 +0530

    Function Definition error fixed

--- mymailer.py
+++ mymailer.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python
-
 import smtplib
-
-sendmail(from_addr = 'trilaciousexps@gmail.com', 
-          to_addr_list = ['dani4cool@gmail.com'],
-          subject = 'Email from Raspberry Pi', 
-          message = 'Hello, this is Raspberry Pi sending you this email....', 
-          login = 'trilaciousexps@gmail.com', 
-          password = 'experiments@tlabs',
-          smtpserver = 'smtp.gmail.com:587')
-
-header = 'From: %s\n' % from_addr
-header += 'To: %s\n' % ','.join(to_addr_list)
-header += 'Cc: %s\n' % ','.join(cc_addr_list)
-header += 'Subject: %s\n\n' % subject
-message = header + message 
-					  
-server = smtplib.SMTP(smtpserver)
-server.starttls()
-server.login(login,password)
-problems = server.sendmail(from_addr, to_addr_list, message)
-server.quit()
-return problems
+ 
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver='smtp.gmail.com:587'):
+    header  = 'From: %s\n' % from_addr
+    header += 'To: %s\n' % ','.join(to_addr_list)
+    header += 'Cc: %s\n' % ','.join(cc_addr_list)
+    header += 'Subject: %s\n\n' % subject
+    message = header + message
+ 
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+    return problems
+ 
+sendemail(from_addr    = 'trilaciousexps@gmail.com', 
+          to_addr_list = ['thoufeeq.mohd@gmail.com'],
+          subject      = 'Email from Raspberry Pi', 
+          message      = 'Hello, this is Raspberry Pi sending you this email....', 
+          login        = 'trilaciousexps@gmail.com', 
+          password     = 'experiments@tlabs')    
+    
