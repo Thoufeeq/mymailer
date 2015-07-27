25532bb73ab743511c03a4d245fc21df7f674919
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2015-07-27 20:06:01 +0530

    Removed header for cc_addr

--- mymailer.py
+++ mymailer.py
@@ -8,7 +8,6 @@ def sendemail(from_addr, to_addr_list,
               smtpserver='smtp.gmail.com:587'):
     header  = 'From: %s\n' % from_addr
     header += 'To: %s\n' % ','.join(to_addr_list)
-    header += 'Cc: %s\n' % ','.join(cc_addr_list)
     header += 'Subject: %s\n\n' % subject
     message = header + message
  
